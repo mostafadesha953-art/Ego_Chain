bf7e284a2dacdb221ea126bcf649e4a639c8e232
mostafadesha953-art <mostafadesha953@gmail.com>
2026-03-01 03:51:41 +0200

    Update secure-token.js

--- secure-token.js
+++ secure-token.js
@@ -3,4 +3,19 @@
     return crypto.createHmac('sha256', SECRET_KEY)
                  .update(${walletAddress}-${amount}-${Date.now()})
                  .digest('hex').substring(0, 8);
+
+}
+
+// secure-token.js
+let maxLimitLocked = false;
+
+export function setMaxSupply(limit) {
+    if (maxLimitLocked) throw new Error("عفواً: سقف العملة مغلق للأبد ولا يمكن تعديله.");
+    
+    // حفظ السقف
+    this.limit = limit;
+    
+    // غلق الإمكانية للأبد (Finalize)
+    maxLimitLocked = true; 
+    return "تم تثبيت السقف بنجاح وقفل التعديل.";
 }
