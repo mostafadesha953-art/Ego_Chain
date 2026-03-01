10bc01d051c701dab8a54a70c68d079b3804a391
mostafadesha953-art <mostafadesha953@gmail.com>
2026-03-01 03:14:32 +0200

    Update auth-system.js

--- src/auth-system.js
+++ src/auth-system.js
@@ -1,4 +1,15 @@
-﻿const fs = require('fs');
+// src/auth-system.js
+function login(identity, password) {
+    // منطق التحقق من الإيميل أو الهاتف
+    console.log(تسجيل الدخول لـ ${identity});
+}
+
+function syncToEgoChain(offlineMinedAmount) {
+    // دمج العملات التي تم تعدينها محلياً مع العقد الذكي عند الرفع
+    console.log(يتم الآن رفع ${offlineMinedAmount} عملة إلى شبكة EGO Chain...);
+}
+
+const fs = require('fs');
 const crypto = require('crypto');
 
 class AuthSystem {
@@ -8,4 +19,5 @@ class AuthSystem {
         return crypto.createHash('sha256').update(nationalID + name).digest('hex');
     }
 }
+
 module.exports = new AuthSystem();
